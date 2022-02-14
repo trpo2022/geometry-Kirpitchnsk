c0ea8a9c95232043b5c6be46f68f5dc307563d63
Vadim Borisov <Kirpitchnsk@users.noreply.github.com>
2022-02-14 10:09:19 +0700

    Create file geometry.h

---
+++ geometry.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  int x1 = 0, y1 = 0, radius = 0;
+  scanf("%d%d%d", &x1, &y1, &radius);
+  printf("circle(%d %d, %d\n)", x1, y1, radius);
+  return 0;
+}

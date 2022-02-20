20983140c9930fd88960896ce14b45a83aba6f0c
Vadim Borisov <Kirpitchnsk@users.noreply.github.com>
2022-02-20 18:29:20 +0700

    Make file geometry.c and write program there

--- geometry.c
+++ geometry.c
@@ -3,6 +3,6 @@
 int main() {
   int x1 = 0, y1 = 0, radius = 0;
   scanf("%d%d%d", &x1, &y1, &radius);
-  printf("circle(%d %d, %d\n)", x1, y1, radius);
+  printf("\ncircle(%d %d, %d)\n", x1, y1, radius);
   return 0;
 }

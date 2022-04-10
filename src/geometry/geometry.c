f39693add457349e38d061568cf97492c5371a78
Kirpitchnsk <98724575+Kirpitchnsk@users.noreply.github.com>
2022-04-11 01:10:59 +0700

    Update geometry.c

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -7,57 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 const double pi = 3.1415926535898;
-int ukazatel(char c[], char p, int i) {
-  int ukaz = 0;
-  while (c[i] != '\0') {
-    if (c[i] == p) {
-      ukaz = i;
-      break;
-    }
-    i++;
-  }
-  return ukaz;
-}
-int countelement(char c[], char p) {
-  int count = 0;
-  int i = 0;
-  while (c[i] != '\0') {
-    if (c[i] == p) {
-      count++;
-    }
-    i++;
-  }
-  return count;
-}
-double side(double x1, double y1, double x2, double y2) {
-  return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-}
-double area_of_circle(double radius) { return pi * pow(radius, 2); }
-double area_of_triangle(double a, double b, double c) {
-  double halfperimeter = (a + b + c) / 2;
-  double area = sqrt(halfperimeter * (halfperimeter - a) * (halfperimeter - b) *
-                     (halfperimeter - c));
-  return area;
-}
-void make_string_null(char c[]) {
-  int i;
-  for (i = 0; i < strlen(c); i++) {
-    c[i] = ' ';
-  }
-}
-bool num_check(char c[]) {
-  int i, k = 0;
-  for (i = ukazatel(c, '(', 0); i < ukazatel(c, ')', 0); i++) {
-    if (!isdigit(c[i]) && !isspace(c[i]) && !ispunct(c[i])) {
-      k++;
-    }
-  }
-  if (k > 0) {
-    return false;
-  } else {
-    return true;
-  }
-}
 int main() {
   FILE *S1;
   int i = 1;
